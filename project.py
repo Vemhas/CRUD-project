147401c5532d7c25748d9f0fe0c0e82dce6a1f9a
Vemund Hassel <vemund.hassel@gmail.com>
2019-06-12 18:03:04 +0800

    Add func for display restaurant name + price

--- project.py
+++ project.py
@@ -1,10 +1,31 @@
 from flask import Flask
 app = Flask(__name__)
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Restaurant, Base, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
-    return "Hello World"
+    restaurants = session.query(Restaurant)
+    output = ''
+    for restaurant in restaurants:
+        output += '<br>'
+        output += restaurant.name
+        items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+        for i in items:
+            output += i.name
+            output += i.price
+            output += '<br>'
+    return output
+    
 
 if __name__ == "__main__":
     app.debug = True
